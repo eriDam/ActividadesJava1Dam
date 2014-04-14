7ae8c57012c315bc93c91da7ed8f2389dee37e49
eriDam <erika.c.caballero@gmail.com>
2014-04-14 15:28:39 +0200

    Actividad 3.5 DefinirUnaConstante

---
+++ Act_3.5DefinirUnaConstante/src/Constante.java
@@ -0,0 +1,16 @@
+    /*DEFINIR UNA CONSTANTE Las variables son unidades basicas de almacenamiento en Java .
+     * Una variable se define por la combinacion de un identificador, un tipo y un 
+     * inicializador opcional. Además las variables tiene un ámbito que define su 
+     * visibilidad y una duracion. 
+     */
+
+public class Constante {	
+	public static void main(String[] args) { 
+	final double IVA=0.21;
+	
+	// TODO Auto-generated method stub
+    //IMPRIMIR UNA CONSTANTE
+		System.out.println(IVA);
+	}
+
+}
